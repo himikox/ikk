2c4bec9a1766a039b2b73b66e19211269658b850
Himikox <ikram.mkaissi@insat.u-carthage.tn>
2021-05-09 00:44:16 +0100

    calendar added

--- screens/ChooseDoctorScreen.js
+++ screens/ChooseDoctorScreen.js
@@ -44,6 +44,11 @@ const ChooseDoctorScreen = ({ navigation, route }) => {
     const [doctorVisible, setDoctorVisible] = useState(true);
     const [personalVisible, setPersonalVisible] = useState(false);
     const [addressVisible, setAddressVisible] = useState(false);
+    const [selectedDate, setSelectedDate] = useState('');
+    const [markedDates, setMarkedDates] = useState({
+        '2021-05-16': {selected: true, marked: true, selectedColor: 'blue'},
+    })
+
     const [firstName, setFirstName] = useState("");
     const [imageUrl, setImageUrl] = useState("");
     const [ loading, setLoading ] = useState(true);
@@ -498,7 +503,7 @@ const ChooseDoctorScreen = ({ navigation, route }) => {
                 }}>
                     <View style={{
 
-
+                        flex:1,
                         width:width*1.2,
                         height:"10%",
                         alignItems:'center',
@@ -530,51 +535,21 @@ const ChooseDoctorScreen = ({ navigation, route }) => {
                             </View>
                         </ImageBackground>
                     </View>
-                    <Calendar
-                        // Initially visible month. Default = Date()
-                        current={'2012-03-01'}
-                        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                        minDate={'2012-05-10'}
-                        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                        maxDate={'2012-05-30'}
-                        // Handler which gets executed on day press. Default = undefined
-                        onDayPress={(day) => {console.log('selected day', day)}}
-                        // Handler which gets executed on day long press. Default = undefined
-                        onDayLongPress={(day) => {console.log('selected day', day)}}
-                        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-                        monthFormat={'yyyy MM'}
-                        // Handler which gets executed when visible month changes in calendar. Default = undefined
-                        onMonthChange={(month) => {console.log('month changed', month)}}
-                        // Hide month navigation arrows. Default = false
-                        hideArrows={true}
-                        // Replace default arrows with custom ones (direction can be 'left' or 'right')
-                        renderArrow={(direction) => (<Arrow/>)}
-                        // Do not show days of other months in month page. Default = false
-                        hideExtraDays={true}
-                        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-                        // day from another month that is visible in calendar page. Default = false
-                        disableMonthChange={true}
-                        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-                        firstDay={1}
-                        // Hide day names. Default = false
-                        hideDayNames={true}
-                        // Show week numbers to the left. Default = false
-                        showWeekNumbers={true}
-                        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-                        onPressArrowLeft={subtractMonth => subtractMonth()}
-                        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-                        onPressArrowRight={addMonth => addMonth()}
-                        // Disable left arrow. Default = false
-                        disableArrowLeft={true}
-                        // Disable right arrow. Default = false
-                        disableArrowRight={true}
-                        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-                        disableAllTouchEventsForDisabledDays={true}
-                        // Replace default month and year title with custom one. the function receive a date as parameter.
-                        renderHeader={(date) => {/*Return JSX*/}}
-                        // Enable the option to swipe between months. Default = false
-                        enableSwipeMonths={true}
-                    />
+                    <View style={{flex:0.5}}>
+
+                    </View>
+                  <View style={{flex:7 ,width:"80%",alignSelf:'center'}}>
+                      <Calendar
+                          onDayPress={(day) => { setSelectedDate(day); setMarkedDates({
+                              [day.dateString]: {selected: true, selectedColor: '#19769F'}});console.log('selected day', markedDates);}}
+                          enableSwipeMonths={true}
+
+                          markedDates={markedDates}
+
+
+                      />
+
+                  </View>
                 </View>
 
                 <TouchableOpacity
